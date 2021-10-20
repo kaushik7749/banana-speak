431b3c25ae376bba537413aa0ba258d819415a46
Kaushik Yadure <kaushikyadure4977@gmail.com>
2021-10-20 19:24:02 +0530

    input outpt process done

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var btnTranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#txt-input")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
 console.log(txtInput);
 
 function clickEventHandler() {
-    console.log("clicked!")
-    console.log("input", txtInput.value);
+    outputDiv.innerText = "pika pika pikachu " + txtInput.value;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler) //this is basically concept of call back function
