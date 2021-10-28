8b8bf835c704e4cfdb219d4b30b354efb5a3a663
Kaushik Yadure <kaushikyadure4977@gmail.com>
2021-10-28 19:46:08 +0530

    fetched api and finally completed the application.

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-console.log(txtInput);
-
-function clickEventHandler() {
-    outputDiv.innerText = "pika pika pikachu " + txtInput.value;
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
 }
 
+function errorHandler(error) {
+    console.log("error occured", error)
+}
+
+function clickEventHandler() {
+    var inputText = txtInput.value; //taking input text
+    //calling server from processing
+    fetch(getTranslationURL(inputText))
+        .then(respone => respone.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
+}
 btnTranslate.addEventListener("click", clickEventHandler) //this is basically concept of call back function
